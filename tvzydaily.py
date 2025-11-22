7e09520fab482f26da2164be91eff6280869bcd7
wangchongzhq <162719366+wangchongzhq@users.noreply.github.com>
2025-11-22 21:20:50 +0800

    Update tvzydaily.py

--- tvzydaily.py
+++ tvzydaily.py
@@ -28,8 +28,9 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# GitHub数据源列表（至少5个）
+# GitHub数据源列表（至少10个）
 GITHUB_SOURCES = [
+    "http://106.53.99.30/2025.txt",
     "http://tv.html-5.me/i/9390107.txt",
     "https://ghcy.eu.org/https://raw.githubusercontent.com/Supprise0901/TVBox_live/refs/heads/main/live.txt",
     "https://ghcy.eu.org/https://raw.githubusercontent.com/ffmking/tv1/main/888.txt",
