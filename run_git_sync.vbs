4665dad048dbeee4da4c9401ec695b0d68d3f1b4
wangchongzhq <bjzhanghongqing@163.com>
2025-12-26 12:43:36 +0800

    update files

--- run_git_sync.vbs
+++
@@ -1,2 +0,0 @@
-Set oShell = CreateObject("WScript.Shell")
-oShell.Run "powershell -ExecutionPolicy Bypass -File ""%~dp0git_sync.ps1""", 1, True
--- unlock_git.ps1
+++
@@ -1,8 +0,0 @@
-# Unlock Git Lock File
-$lockFile = ".git\index.lock"
-if (Test-Path $lockFile) {
-    Remove-Item $lockFile -Force
-    Write-Host "[OK] Lock file cleaned" -ForegroundColor Green
-} else {
-    Write-Host "[OK] No lock file found" -ForegroundColor Green
-}
